d8432ce9817644f370b62726a0eb9c7c55be8979
Polawat Krates <fuefolkjj1230@gmail.com>
2026-01-09 15:28:35 +0700

    Finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String activeDice = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      var dice = Random().nextInt(6) + 1;
+      activeDice = 'assets/images/dice-$dice.png';
+      print(activeDice);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.white, Colors.blue, Colors.green],
+        body: SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.white, Colors.blue, Colors.green],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(activeDice),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: EdgeInsets.all(16),
+                  ),
+                  onPressed: rollDice,
+                  child: Text("Roll the Dice"),
+                ),
+              ],
             ),
           ),
         ),
